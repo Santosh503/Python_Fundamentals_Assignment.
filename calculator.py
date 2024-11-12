8829e1d0884d06399dbb8ef7a57b0d87abed4c8e
Santosh Bhatta <santosh_btechedit2023@kusoed.edu.np>
2024-11-12 08:42:41 +0545

    completed task 3

---
+++ calculator.py
@@ -0,0 +1,67 @@
+import time
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is not allowed."
+    return a / b
+
+def calculator():
+    print("Simple Calculator")
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    
+    while True:
+        choice = input("Enter choice (1/2/3/4) or 'exit' / 'Exit' to quit: ")
+        
+        if choice.lower() == "exit":
+            print("Exiting the calculator.")
+            time.sleep(1)   # to delay program for given time 
+            print("1")
+            time.sleep(1) 
+            print("2")
+            time.sleep(1)
+            print("3")
+            time.sleep(1)
+            print("Good Bye!!")
+            break
+
+        if choice in ('1', '2', '3', '4'):
+            try:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+
+                if choice == '1':
+                    result = add(num1, num2)
+                    print(f"The result is: {result}")
+
+                elif choice == '2':
+                    result = subtract(num1, num2)
+                    print(f"The result is: {result}")
+
+                elif choice == '3':
+                    result = multiply(num1, num2)
+                    print(f"The result is: {result}")
+
+                elif choice == '4':
+                    result = divide(num1, num2)
+                    print(f"The result is: {result}")
+
+            except ValueError:
+                print("Invalid input. Please enter numeric values.")
+
+        else:
+            print("Invalid choice. Please select a valid operation.")
+
+calculator()
